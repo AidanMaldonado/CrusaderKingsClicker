47a332307a0df311bc00b72e99526fc7fad3d144
Aidan Maldonado <aidanm1198@gmail.com>
2020-11-09 14:52:57 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+Crusader kings game where you have heirs and they die and you need to conquer the kingdom.
+YOu have to click to get resources and make decisions.
+
+
+-- MVP --
+Screens
+Map/ Resources Screen/ Kings Court/ Family tree
+
+Population, food, materials
+Can name kingdom
+-Cannot name Kings, randomly generated
+
+some scripted experiences
+random events
+
+conquer where you are at
+
+--Later updates
+
+procedurally generate maps
+you can die/succeeded by heir
+*/
